15f2cc1119dbab31ec6d9f9b085a73cef421be07
Marcos Vinicius <40502247+Marcosultymos@users.noreply.github.com>
2023-07-16 09:30:23 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -114,16 +114,7 @@ const cmtSyntax = {
 
 // Comment syntax for double-line comments
 const cmtSyntaxDouble = {
-  c: { first: "/* ", last: " */" },
-  cc: { first: "/* ", last: " */" },
-  cpp: { first: "/* ", last: " */" },
-  cxx: { first: "/* ", last: " */" },
-  h: { first: "/* ", last: " */" },
   html: { first: "<!-- ", last: " -->" },
-  hpp: { first: "/* ", last: " */" },
-  hxx: { first: "/* ", last: " */" },
-  inl: { first: "/* ", last: " */" },
-  ipp: { first: "/* ", last: " */" },
   twig: { first: "{# ", last: " #}" },
   blade: { first: "{{-- ", last: " --}}" },
   hbs: { first: "{{!-- ", last: " --}}" },
@@ -131,7 +122,6 @@ const cmtSyntaxDouble = {
   mako: { first: "<%# ", last: " %>" },
   mst: { first: "<!-- ", last: " -->" },
   mustache: { first: "<!-- ", last: " -->" },
-  py: { first: "''' ", last: " '''" },
   tpl: { first: "{* ", last: " *}" },
   xml: { first: "<!-- ", last: " -->" },
   liquid: { first: "{# ", last: " #}" },
